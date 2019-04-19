e9b498800908b22aaf1aa3d500ad46f70293d910
JimyHsu <airball91327@gmail.com>
2019-04-19 17:51:41 +0800

    1. Designing Models.

---
+++ EDIS/EDIS/Areas/BMED/Models/RepairModels/Ticket_seq_tmpModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EDIS.Areas.BMED.Models
+{
+    public class Ticket_seq_tmpModel
+    {
+        [Key]
+        public string YYYMM { get; set; }
+        public string TICKET_SEQ { get; set; }
+    }
+}
